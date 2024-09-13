6dc7f2fa42462460f296035569b973e9c2b1baea
rnjsrkdud0524 <rjanseh2003@naver.com>
2024-09-13 20:55:00 +0900

    스터디02

--- myweb/example/urls.py
+++ myweb/example/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path, include
-from .views import HelloAPI
+from .views import HelloAPI, BookAPI, BooksAPI, bookAPI, booksAPI, BookAPIMixins, BooksAPIMixins, BookViewSet
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register('books', BookViewSet)
+
+urlpatterns = router.urls
 
 urlpatterns = [
-    path('hello/', HelloAPI),
+    path("hello/", HelloAPI),
+    path("fbv/books/", booksAPI),
+    path("fbv/book/<int:bid>", bookAPI),
+    path("cbv/books/", BooksAPI.as_view()),
+    path("cbv/book/<int:bid>", BookAPI.as_view()),
+    path("mixin/books/", BooksAPIMixins.as_view()),
+    path("mixin/book/<int:bid>/", BookAPIMixins.as_view())
 ]
